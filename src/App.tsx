0ce7fd8d53a2b6ed8941e934e404cbc83c2bc36d
NoobAdmin <hchouhan3654@outlook.com>
2026-02-24 20:40:00 +0530

    init: Add base setup

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col app_primary'>
+      {/* header */}
+      <div className='app_header'>
+
+        <img src='https://www.uniper.energy/themes/custom/uniper2025/logo.svg' alt='uniper'/>
+        <div className='header_content'>
+          <h4>Test Setup</h4>
+        </div>
+      </div>
+
+      {/* Body */}
+      <div className='app_body'>
+        <p>Your tasks goes here</p>
+
+      </div>
+
+      {/* Footer */}
+      <div className='app_footer'>
+        <p>All the best !!!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
