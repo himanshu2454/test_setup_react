0cb2320a00d6eebd66481d1fcd0b1a0be287d7df
NoobAdmin <hchouhan3654@outlook.com>
2026-02-24 20:49:35 +0530

    chore: remove tailwind configs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className='flex flex-col app_primary'>
+    <div className='app_primary'>
       {/* header */}
       <div className='app_header'>
 
