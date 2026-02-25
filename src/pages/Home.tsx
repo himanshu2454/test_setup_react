406ec1c3ee10c5b9d371b3fce9d6118b5a45baf9
NoobAdmin <hchouhan3654@outlook.com>
2026-02-25 12:53:14 +0530

    typo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <h4>Instruction -</h4>
       <ul>
         <li>Update Test page /test to submit your solution</li>
-        <li>Click on Task button to complete individual tasks.</li>
+        <li>Click on Task button to view individual tasks.</li>
       </ul>
     </div>
   )
